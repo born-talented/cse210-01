018babcd478520d67f124fcac933c223609ab80c
Adil Rafi <adil793@hotmail.com>
2022-04-28 15:18:40 +0500

    Finally main function

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,20 +1,29 @@
 #Tic-Tac-Toe game code
 
-
+def main():
+    player = next_player("")
+    board = create_board()
+    while not (has_winner(board) or is_a_draw(board)):
+        display_board(board)
+        make_move(player, board)
+        player = next_player(player)
+    display_board(board)
+    print("Good game. Thanks for playing!")
+    
 
 
 def create_board():
     board = []
     for square in range(9):
-        board.append(square = 1)
+        board.append(square + 1)
     return board
 
 def display_board(board):
     print()
     print(f"{board[0]}|{board[1]}|{board[2]}")
-    print('-------')
+    print('-----')
     print(f"{board[3]}|{board[4]}|{board[5]}")
-    print('-------')
+    print('-----')
     print(f"{board[6]}|{board[7]}|{board[8]}")
     print()
 
@@ -43,3 +52,6 @@ def next_player(current):
         return "x"
     elif current == "x":
         return "o"
+
+if __name__ == "__main__":
+    main()
