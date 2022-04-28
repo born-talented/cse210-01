675f043b9abb1e0b5e873d8d6e3a6b724068f5ff
Adil Rafi <adil793@hotmail.com>
2022-04-28 15:01:54 +0500

    I have wriiten is_a_draw & has_winner functions

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -17,3 +17,19 @@ def display_board(board):
     print('-------')
     print(f"{board[6]}|{board[7]}|{board[8]}")
     print()
+
+def is_a_draw(board):
+    for square in range(9):
+        if board[square] != "x" and board[square] != "o":
+            return False
+    return True
+
+def has_winner(board):
+    return (board[0] == board[1] == board[2] or
+            board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] or
+            board[0] == board[3] == board[6] or
+            board[1] == board[4] == board[7] or
+            board[2] == board[5] == board[8] or
+            board[0] == board[4] == board[8] or
+            board[2] == board[4] == board[6])
